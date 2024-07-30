f0a6703d2e33f8420ce9eb7c56cd59306d239b14
jh77040 <163450770+jh77040@users.noreply.github.com>
2024-07-30 17:24:18 +0000

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import NavBar from './NavBar';
+import Home from './Home';
 
 export default function App() {
   const title = "Welcome to the blog";
@@ -8,17 +9,9 @@ export default function App() {
 
   return (
     <div className = "App">
-      <NavBar></NavBar>
+      <NavBar />
       <div className = "content">
-        <h1>{ title }</h1>
-        <p>Liked {likes} times</p>
-        <p> {10} </p>
-        <p>{ "Hello neighbors" }</p>
-        <p>{ [1, 2, 3,4, 5] }</p>
-        <p>{ Math.random() * 10 }</p>
-
-        <a href = {link}>Google Site</a
-                                    >
+        <Home />
       </div>
     </div>
   );
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,7 +1,7 @@
 const NavBar = () => {
   return (
     <nav className='navbar'>
-      <h1>Whatever This Blog is Supposed to be Called</h1>
+      <h1>HomeBlog</h1>
       <div className="links">
         <a href="/">Home</a>
         <a href="/create">New Blog</a>
