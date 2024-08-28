7531bcb2b65b1a80e709478206944a3da52b0dab
jh77040 <163450770+jh77040@users.noreply.github.com>
2024-08-28 19:57:53 +0000

    clicker

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,18 @@
 const Home = () => {
+
+  const handleClick = () => {
+    console.log("Hello, User");
+  }
+
+  const handleClickAgain = (name) => {
+    console.log("Hello, " + name);
+  }
+  
   return (
     <div className = "home">
       <h2>Homepage</h2>
+      <button onClick={handleClick()}>Click Me</button>
+      <button onClick={handleClickAgain('name')}>Click Me Again</button>
     </div>
   )
 }
