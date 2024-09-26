397a1e517b18b6a3a41fa61abf18e2a058e88c2a
jh77040 <163450770+jh77040@users.noreply.github.com>
2024-09-26 20:06:16 +0000

    name change

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,6 +8,8 @@ const Home = () => {
     { title: 'Web dev tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ]);
 
+  const [name, setName] = useState('mario');
+
   const handleDelete = (id) =>  {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
@@ -15,13 +17,14 @@ const Home = () => {
 
   useEffect(() => {
     console.log("use effect ran");
-    console.log(blogs);
-  });
+    console.log(name);
+  }, [name]);
 
   return (
     <div className = "home">
-      <BlogList blogs = {blogs} title = "All Blogs!" />
-      <BlogList blogs = {blogs.filter((blog) => blog.author === 'mario')} title = "Mario's Blogs!" />
+      <BlogList blogs = {blogs} title = "All Blogs!" handleDelete={handleDelete} />
+      <button onClick={() => setName('luigi')}>change name</button>
+      <p>{ name }</p>
     </div>
   )
 }
