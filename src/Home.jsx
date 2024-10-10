97ec9f7866e8e01aa524d595e4e39772f4224e8f
jh77040 <163450770+jh77040@users.noreply.github.com>
2024-10-10 04:04:51 +0000

    updates

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,11 +10,6 @@ const Home = () => {
 
   const [name, setName] = useState('mario');
 
-  const handleDelete = (id) =>  {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
-
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then(res => {
@@ -28,9 +23,7 @@ const Home = () => {
 
   return (
     <div className = "home">
-      <BlogList blogs = {blogs} title = "All Blogs!" handleDelete={handleDelete} />
-      <button onClick={() => setName('luigi')}>change name</button>
-      <p>{ name }</p>
+      {blogs && <BlogList blogs = {blogs} title = "All Blogs!"/>}
     </div>
   )
 }
