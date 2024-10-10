eeb35e6cf5bfcae892369dae4919799e7fe76f08
jh77040 <163450770+jh77040@users.noreply.github.com>
2024-10-10 04:36:31 +0000

    useFetch2

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,13 +1,13 @@
 import{ useState, useEffect, } from 'react'
 
-const useFetch = () => {
+const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
   
    useEffect(() => {
      setTimeout(() => {
-       fetch('http://localhost:8000/blogs')
+       fetch()
          .then(res => {
            if(!res.ok) {
              throw Error("could not fetch data for that resource");
@@ -24,7 +24,10 @@ const useFetch = () => {
            setError(err.message);
          })
      }, 1000);
-   }, []);
+   }, [url]);
+
+return { data, isPending, error } 
+  
 }
 
 export default useFetch;
