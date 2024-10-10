5652c3635842b0a596e06f9020d35653b3192f31
jh77040 <163450770+jh77040@users.noreply.github.com>
2024-10-10 21:08:25 +0000

    input

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
 
 const Create = () => {
-  const [title, setTitle] = useState();
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('');
   
   return (
     <div className = "create">
@@ -11,17 +13,26 @@ const Create = () => {
         <input 
           type="text"
           required
+          value = {title}
+          onChange = {(e) => setTitle(e.target.value)}
         />
         <label>Blog body:</label>
         <textarea
           required
+          value = {body}
+          onChange = {(e) => setBody(e.target.value)}
         ></textarea> 
         <label>Blog author</label>
-        <select>
+        <select
+          value = {author}
+          onChange = {(e) => setAuthor(e.target.value)}>
           <option value= "mario">Mario</option>
-          <option value= "yoshi">Yoshi</option>"
+          <option value= "yoshi">Yoshi</option>
         </select>
         <button>Add Blog</button>
+        <p>{ title }</p>
+        <p>{ body }</p>
+        <p>{ author }</p>
       </form>
     </div>
   );
